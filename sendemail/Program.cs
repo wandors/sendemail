e78f6ea7addd655992e7ea49f3e456b0c1d486d3
Sergey <srpolunec@gmail.com>
2019-01-13 15:06:22 +0200

    Добавьте файлы проекта.

---
+++ sendemail/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace sendemail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MailMessage mail = new MailMessage();
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            mail.From = new MailAddress("srpolunec@gmail.com");
+            mail.To.Add("polunec@ukr.net");
+            mail.Subject = "Test Mail - 1";
+            mail.Body = "mail with attachment";
+            System.Net.Mail.Attachment attachment;
+            attachment = new System.Net.Mail.Attachment("d:/textfile.txt");
+            mail.Attachments.Add(attachment);
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("srpolunec@gmail.com", "sr250384");
+            SmtpServer.EnableSsl = true;
+
+            SmtpServer.Send(mail);
+        }
+    }
+}
